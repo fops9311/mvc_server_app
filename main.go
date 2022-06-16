355bfc6a2e9b3d973dd17258830f3a46e9df9805
home_ubuntu <fops9311@yandex.ru>
2022-06-16 21:28:41 +0300

    basic layout 2

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fops9311/mvc_server_app/router"
+)
+
+func main() {
+	router.Hello()
+	fmt.Print("Hi")
+}
---
+++ router/router.go
@@ -0,0 +1,9 @@
+package router
+
+import (
+	"fmt"
+)
+
+func Hello() {
+	fmt.Print("Hi\n")
+}
