a8a9941c3fe7afe1beccb31b61eb231df5e811bf
home_ubuntu <fops9311@yandex.ru>
2022-06-19 22:13:46 +0300

    added makeParams func to convert url par to map

--- servers/echo_server/echo_server.go
+++ servers/echo_server/echo_server.go
@@ -9,43 +9,51 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+func makeParams(c echo.Context) (params map[string]string) {
+	params = make(map[string]string)
+	for _, p := range c.ParamNames() {
+		params[p] = c.Param(p)
+		//fmt.Printf("key=%s value=%s", p, c.Param(p))
+	}
+	return params
+}
+
 type Echo_server struct {
 	e *echo.Echo
 }
 
-//wtf
 func (er *Echo_server) Init() server.Server {
 	er.e = echo.New()
 	return er
 }
 func (er *Echo_server) AddAction(Act resource.ActionPath) (err error) {
+
 	switch Act.Verb {
 	case "GET":
 		er.e.GET(Act.Path, func(c echo.Context) error {
-			res, _ := Act.Action(make(map[string]string))
+			res, _ := Act.Action(makeParams(c))
 			return c.String(http.StatusOK, res)
 		})
 		fmt.Printf("Path %s added\n", Act.Path)
 	case "POST":
 		er.e.POST(Act.Path, func(c echo.Context) error {
-			res, _ := Act.Action(make(map[string]string))
+			res, _ := Act.Action(makeParams(c))
 			return c.String(http.StatusOK, res)
 		})
 		fmt.Printf("Path %s added\n", Act.Path)
 	case "PUT":
 		er.e.PUT(Act.Path, func(c echo.Context) error {
-			res, _ := Act.Action(make(map[string]string))
+			res, _ := Act.Action(makeParams(c))
 			return c.String(http.StatusOK, res)
 		})
 		fmt.Printf("Path %s added\n", Act.Path)
 	case "DELETE":
 		er.e.DELETE(Act.Path, func(c echo.Context) error {
-			res, _ := Act.Action(make(map[string]string))
+			res, _ := Act.Action(makeParams(c))
 			return c.String(http.StatusOK, res)
 		})
 		fmt.Printf("Path %s added\n", Act.Path)
 	}
-
 	return nil
 }
 func (er *Echo_server) Serve(port string) (err error) {
