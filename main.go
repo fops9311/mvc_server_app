d379f39d40fdd455d5cdb9b3deda8a0b290bdc97
home_ubuntu <fops9311@yandex.ru>
2022-06-19 15:09:29 +0300

    work in progress

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 //go:generate echo "creating some files, based on resource spec"
-//go:generate go run ./gogenerate/mvcgen.go page user object
+//go:generate go run ./gogenerate/mvcgen.go object
 package main
 
 import (
-	"fmt"
-
-	"github.com/fops9311/mvc_server_app/router"
+	"github.com/fops9311/mvc_server_app/controllers/object_controller"
+	"github.com/fops9311/mvc_server_app/model/server"
+	"github.com/fops9311/mvc_server_app/servers/echo_server"
 )
 
+var r server.Server = &echo_server.Echo_server{}
+
 func main() {
-	router.Hello()
-	fmt.Print("Hi")
+	r = r.Init()
+	res := object_controller.Resource
+	r.AddResurce(res, "")
+	r.Serve("8000")
 }
