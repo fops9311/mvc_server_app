7146ac6e5782f88a2d6097e542dc83d2123a0a8e
home_ubuntu <fops9311@yandex.ru>
2022-06-23 08:34:49 +0300

    minor changes

--- main.go
+++ main.go
@@ -3,21 +3,26 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/fops9311/mvc_server_app/controllers/page_controller"
 	"github.com/fops9311/mvc_server_app/model/resource"
 	"github.com/fops9311/mvc_server_app/model/server"
 	"github.com/fops9311/mvc_server_app/servers/echo_server"
+	"github.com/fops9311/mvc_server_app/tassets"
 )
 
 var r server.Server = &echo_server.Echo_server{}
 
 func main() {
+	//tassets.Init()
+	fmt.Println(tassets.GetAsset("tassets/test/test.txt"))
 	r = r.NewServer()
 	root := resource.NewResource()
 	root.Key = "/v1"
-	home := page_controller.Resource
-	home.Key = "/home"
-	root.Children["home"] = home
+	pages := page_controller.Resource
+	pages.Key = "/pages"
+	root.Children["pages"] = pages
 	r.AddResurce(root, "")
 	r.Serve("8000")
 }
